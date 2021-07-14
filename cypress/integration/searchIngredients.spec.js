285bf1fb75b86f748107a3cba7a1271ab19e5a0e
Alan Benavides <bruno@coastt.org>
2021-07-14 19:40:56 -0400

    add: update test

--- cypress/integration/searchIngredients.spec.js
+++ cypress/integration/searchIngredients.spec.js
@@ -1,16 +1,16 @@
-describe('Comecon App', () => {
-    beforeEach(() => {
-        cy.visit('/')
-    })
+describe("Comecon App", () => {
+	beforeEach(() => {
+		cy.visit("/");
+	});
 
-    describe('Test Case 22', () => {
-        it('Verify that the text in the search bar have matches', () => {
-            const serachWord = 'que'
-            cy.get('input.MuiInputBase-input').type(serachWord)
-            cy.wait(2000)
-            cy.get('ul.MuiAutocomplete-listbox > li').should(($lis) => {
-                expect($lis.eq(0).text()).to.match(RegExp(serachWord))
-            })
-        })
-    })
-})
+	describe("Test Case 22", () => {
+		it("Verify that the text in the search bar have matches", () => {
+			const serachWord = "lec";
+			cy.get("input.MuiInputBase-input").type(serachWord);
+			cy.wait(2000);
+			cy.get("ul.MuiAutocomplete-listbox > li").each(($li) => {
+				expect($li.text()).to.match(RegExp(serachWord));
+			});
+		});
+	});
+});
